8adc04135ed62822c33cb8b47dec3d580fd4f98c
Minh <minbui54@gmail.com>
2020-04-20 17:49:52 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { render } from "react-dom";
+import { Router, Link } from "@reach/router";
+import Details from "./Details";
+import SearchParams from "./SearchParams";
+import ThemeContext from "./ThemeContext";
+
+const App = () => {
+  const theme = useState("darkblue");
+
+  return (
+    <React.StrictMode>
+      <ThemeContext.Provider value={theme}>
+        <div id="something-important">
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+          ;
+        </div>
+      </ThemeContext.Provider>
+    </React.StrictMode>
+  );
+};
+
+render(<App />, document.getElementById("root"));
